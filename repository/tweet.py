6df8c0bd0c459e51bbb4108d6a3cd7f35e96f8b4
André Carvalho <alac1984@gmail.com>
2022-12-06 23:03:15 -0300

    insert_tweet tests finished

--- repository/tweet.py
+++ repository/tweet.py
@@ -1,48 +1,9 @@
-from sqlalchemy import insert
 from sqlalchemy.orm import Session
 
 from entities.tweet import EntityTweet
-from entities.tweet import EntityDomain
-from entities.tweet import EntityEntity
 from .models.tweet import Tweet
 from .models.tweet import Domain
 from .models.tweet import Entity
-from .models.tweet import tb_tweet_domain
-from .models.tweet import tb_tweet_entity
-
-
-def insert_domain(domain: EntityDomain, session: Session):
-    model_domain = Domain(
-        id=domain.id, name=domain.name, description=domain.description
-    )
-    session.add(model_domain)
-    session.commit()
-
-
-def insert_entity(entity: EntityEntity, session: Session):
-    model_entity = Entity(
-        id=entity.id, name=entity.name, description=entity.description
-    )
-    session.add(model_entity)
-    session.commit()
-
-
-def insert_domain_tweet_relation(domain_id: int, tweet_id: int, session: Session):
-    ins = insert(tb_tweet_domain).values(
-        tweet_id=tweet_id,
-        domain_id=domain_id,
-    )
-    session.execute(ins)
-    session.commit()
-
-
-def insert_entity_tweet_relation(entity_id: int, tweet_id: int, session: Session):
-    ins = insert(tb_tweet_entity).values(
-        tweet_id=tweet_id,
-        entity_id=entity_id,
-    )
-    session.execute(ins)
-    session.commit()
 
 
 def insert_tweet(tweet: EntityTweet, session: Session):
@@ -56,13 +17,21 @@ def insert_tweet(tweet: EntityTweet, session: Session):
 
     if tweet.entities:
         for entity in tweet.entities:
-            insert_entity(entity, session)
-            insert_entity_tweet_relation(tweet.id, entity.id, session)
+            model_entity = Entity(
+                id=entity.id,
+                name=entity.name,
+                description=entity.description,
+            )
+            model_tweet.entities.append(model_entity)
 
     if tweet.domains:
         for domain in tweet.domains:
-            insert_domain(domain, session)
-            insert_domain_tweet_relation(tweet.id, domain.id, session)
+            model_domain = Domain(
+                id=domain.id,
+                name=domain.name,
+                description=domain.description,
+            )
+            model_tweet.domains.append(model_domain)
 
     session.add(model_tweet)
     session.commit()
--- tests/test_repository/test_tweet.py
+++ tests/test_repository/test_tweet.py
@@ -1,65 +1,46 @@
 import pytest
 from datetime import datetime
-from sqlalchemy.exc import IntegrityError
-from sqlalchemy.sql import text
 
 from repository.models.tweet import Tweet
-from repository.models.tweet import Domain
-from repository.models.tweet import Entity
 from entities.tweet import EntityTweet
 from entities.tweet import EntityDomain
 from entities.tweet import EntityEntity
 from repository.tweet import insert_tweet
-from repository.tweet import insert_domain
-from repository.tweet import insert_entity
-from repository.tweet import insert_domain_tweet_relation
-from repository.tweet import insert_entity_tweet_relation
 
 
-def test_insert_domain(session):
-    entity_domain = EntityDomain(
+@pytest.fixture
+def full_tweet():
+    entity_domain1 = EntityDomain(
         id=1,
         name="first_domain",
         description="description",
     )
-    insert_domain(entity_domain, session)
-
-    query_domain = session.query(Domain).filter(Domain.id == 1).first()
-
-    assert query_domain.id == 1
-
-
-def test_insert_domain_already_exist(session, init_db):
-    with pytest.raises(IntegrityError):
-        entity_domain = EntityDomain(
-            id=1,
-            name="first_domain",
-            description="description",
-        )
-        insert_domain(entity_domain, session)
-
-
-def test_insert_entity_already_exist(session, init_db):
-    with pytest.raises(IntegrityError):
-        entity_entity = EntityEntity(
-            id=1,
-            name="first_entity",
-            description="description",
-        )
-        insert_entity(entity_entity, session)
-
-
-def test_insert_entity(session):
-    entity_entity = EntityEntity(
+    entity_domain2 = EntityDomain(
+        id=2,
+        name="first_domain",
+        description="description",
+    )
+    entity_entity1 = EntityEntity(
         id=1,
         name="first_entity",
         description="description",
     )
-    insert_entity(entity_entity, session)
+    entity_entity2 = EntityEntity(
+        id=2,
+        name="first_entity",
+        description="description",
+    )
+    entity_tweet = EntityTweet(
+        id=1,
+        text="This is a tweet",
+        created_at=datetime(2022, 1, 1, 1, 1, 1, 1),
+        user_id=1,
+        lang="en",
+        domains=[entity_domain1, entity_domain2],
+        entities=[entity_entity1, entity_entity2],
+    )
 
-    query_entity = session.query(Entity).filter(Entity.id == 1).first()
-
-    assert query_entity.id == 1
+    return entity_tweet
 
 
 def test_insert_tweet_no_domain_no_entity(session):
@@ -78,47 +59,10 @@ def test_insert_tweet_no_domain_no_entity(session):
 
 
 def test_insert_tweet_with_domain_and_entity(full_tweet, session):
-    entity_domain = EntityDomain(
-        id=1,
-        name="first_domain",
-        description="description",
-    )
-    entity_entity = EntityEntity(
-        id=1,
-        name="first_entity",
-        description="description",
-    )
-    entity_tweet = EntityTweet(
-        id=1,
-        text="This is a tweet",
-        created_at=datetime(2022, 1, 1, 1, 1, 1, 1),
-        user_id=1,
-        lang="en",
-        domains=[entity_domain],
-        entities=[entity_entity],
-    )
-    insert_tweet(entity_tweet, session)
+    insert_tweet(full_tweet, session)
 
     query_tweet = session.query(Tweet).filter(Tweet.id == 1).first()
 
     assert query_tweet.id == 1
-
-
-def test_insert_domain_tweet_relation(session, engine, init_db):
-    insert_domain_tweet_relation(1, 2, session)
-
-    with engine.connect() as conn:
-        sel_stmt = text("select * from tb_tweet_domain where tweet_id = :tweet_id")
-        result = conn.execute(sel_stmt, {"tweet_id": 2}).fetchone()
-
-    assert result == (2, 1)
-
-
-def test_insert_entity_tweet_relation(session, engine, init_db):
-    insert_entity_tweet_relation(1, 2, session)
-
-    with engine.connect() as conn:
-        sel_stmt = text("select * from tb_tweet_entity where tweet_id = :tweet_id")
-        result = conn.execute(sel_stmt, {"tweet_id": 2}).fetchone()
-
-    assert result == (2, 1)
+    assert len(query_tweet.domains) == 2
+    assert len(query_tweet.entities) == 2
