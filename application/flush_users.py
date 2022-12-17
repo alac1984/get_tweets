94790d02dbda0e3273982f735a660362d1c19104
André Carvalho <alac1984@gmail.com>
2022-12-17 00:57:05 -0300

    Correct users

--- application/flush_users.py
+++ application/flush_users.py
@@ -7,35 +7,37 @@ def create_users():
     users = [
         {
             "twitter_id": 1420152235145797632,
-            "created_at": "",
-            "username": "",
+            "created_at": "2021-07-27T22:41:05.000Z",
+            "username": "pythonjazz",
             "last_scraped": True,
         },
         {
             "twitter_id": 575522002,
-            "created_at": "",
-            "username": "",
+            "created_at": "2012-05-09T17:06:03.000Z",
+            "username": "Bruna_Hawk",
         },
         {
-            "twitter_id": 1603953685205336065,
-            "created_at": "",
-            "username": "",
+            "twitter_id": 896171046,
+            "created_at": "2012-10-21T21:15:00.000Z",
+            "username": "rebecamaia_p",
         },
         {
             "twitter_id": 313074549,
-            "created_at": "",
-            "username": "",
+            "created_at": "2011-06-08T03:21:19.000Z",
+            "username": "dorafigueiredo",
         },
         {
             "twitter_id": 30541996,
-            "created_at": "",
-            "username": "",
+            "created_at": "2009-04-11T22:38:58.000Z",
+            "username": "dai_tama",
         },
     ]
 
     req = Requisition(payload=users)
 
-    flush_users(req, session)
+    response = flush_users(req, session)
+
+    return response
 
 
 if __name__ == "__main__":
