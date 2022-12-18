841559905725a815a199233695fc23705acb733e
André Carvalho <alac1984@gmail.com>
2022-12-17 23:47:31 -0300

    Change fields in twitter external api

--- external_apis/twitter.py
+++ external_apis/twitter.py
@@ -10,7 +10,7 @@ def make_auth_header():
 def retrieve_tweets_from_user(user: str):
     auth_header = make_auth_header()
     response = requests.get(
-        f"https://api.twitter.com/2/tweets/search/recent?query=from:{user}",
+        f"https://api.twitter.com/2/tweets/search/recent?query=from:{user}&tweet.fields=lang,author_id,context_annotations,created_at,entities",
         headers=auth_header,
     )
     return response.json() if response.content is not None else []
